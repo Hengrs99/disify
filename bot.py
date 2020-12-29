c556c4812e2c12e92a8d981c31c6c7d349b9d38b
Hengrs <hengrs99@seznam.cz>
2020-12-29 16:52:26 +0000

    added channel commands to bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,35 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
-load_dotenv
-TOKEN = os.getenv('DISCORD TOKEN')
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="#", help_command=None)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-client.run(TOKEN)
+
+@bot.command(name='join')
+async def connect(ctx):
+    try:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+        await ctx.send(f"Connected to {channel.name}")
+    except AttributeError:
+        await ctx.send("You need to connect to the voice channel before asking for my presence!")
+
+
+@bot.command(name='leave')
+async def leave(ctx):
+    try:
+        await ctx.voice_client.disconnect()
+    except AttributeError:
+        await ctx.send("I'm not connected to any channel you could kick me out of!")
+
+
+bot.run(TOKEN)
